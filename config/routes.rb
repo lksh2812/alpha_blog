091841fd33a5cefb728157b5db1fb6fc7f303637
lokesh <lksh2812@gmail.com>
2020-05-01 08:35:38 +0530

    created users sign up form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'pages#home'
+
+  get 'signup', to: 'users#new', as: 'users_new'
+  resources :users, except: [:new]
 end
