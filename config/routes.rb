66d980e2478ab37d0ad98ad375df77d1c54addc6
lokesh <lksh2812@gmail.com>
2020-05-01 09:06:12 +0530

    created users create controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  get 'signup', to: 'users#new', as: 'users_new'
+  get 'signup', to: 'users#new', as: 'users_signup'
   resources :users, except: [:new]
 end
